8e58b35a8c4e231ddb222230d83654d9fc9835ca
fabrizzioorderique <fabrizzioorderique@gmail.com>
2020-06-17 17:36:41 -0400

    fixed blurred/error images

--- app/src/main/java/com/example/paintcanvas/MainActivity.java
+++ app/src/main/java/com/example/paintcanvas/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
-    private static int SPLASH_TIME_OUT = 3000;
+    private static int SPLASH_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
